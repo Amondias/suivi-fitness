cf2a14aa519f207465e9b5c630d7d035418b1d9e
ASKIA0SN <imanyxformation@gmail.com>
2026-01-17 13:39:55 +0000

    Mise en place auth sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,38 +2,27 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Laravel\Sanctum\HasApiTokens;
+
 use App\Models\Subscriptions;
-use App\Models\SubscriptionPlans;
 use App\Models\Payments;
 use App\Models\UserPrograms;
 use App\Models\Programs;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
